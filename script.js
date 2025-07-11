813ab0b82871e8ecf24ee3176f3d03a00ea6a305
sicxx-xnx <hpeyton33@gmail.com>
2025-07-11 13:37:23 -0500

    creat a player select prompt and game logic set up a way for the human play to select rock, paper or scissors then set up the inital game logic. so far if human selects rock it will compare to what the computer selected and following the rules of rock paper scissors will tel lyou if you won or lost

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let computerPlayerSelection ;
+let humanChoice ;
+let humanScore = 0
+let computerScore = 0   
+let computerChoice ;
+
 
 function getComputerChoice() {
 let computerChoiceLogic = Math.random()
@@ -6,12 +11,48 @@ if (computerChoiceLogic <= 0.33 ) {
 return computerPlayerSelection = "rock"    
 } else if (computerChoiceLogic > 0.33 && computerChoiceLogic <= 0.66) {
   return computerPlayerSelection = "paper"  
-} else if (computerChoiceLogic > 0.55) {
+} else if (computerChoiceLogic > 0.66) {
   return computerPlayerSelection = "Scissors"  
 } else { computerPlayerSelection = "Something went wrong try again"    
 } {    
 } {    
 }  
-console.log(computerPlayerSelection)
+}
+function getHumanChoice() {
+humanChoice = prompt("choose rock, paper, or Scissors!")    
+    return humanChoice
+}
 
+function playRound(humanChoice,computerChoice) {
+humanChoice = getHumanChoice()  
+computerChoice = getComputerChoice()
+humanChoice.toLowerCase()
+computerChoice.toLowerCase()
+console.log(humanChoice)
+console.log(computerChoice) 
+if (humanChoice === "rock" && computerChoice === "rock") {
+    return console.log("its a tie!")    
+} else if (humanChoice === "rock" && computerChoice === "paper") {
+    return console.log("You Lose!")
+} else if (humanChoice === "rock" && computerChoice === "scissors") {
+    return console.log('You win!')
+} else if (humanChoice === "paper" && computerChoice === "paper" ) {
+    return console.log("its a tie") 
+} else {
+    
+} {
+    
+} {
+    
+} {
+    
+} 
+}
+
+
+function test() {
+    getComputerChoice()
+    getHumanChoice()
+    console.log(computerPlayerSelection)
+    console.log(humanChoice)
 }
