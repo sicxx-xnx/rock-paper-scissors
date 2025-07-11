b18bc41a0a7212be68f932284a079c924f452d9c
sicxx-xnx <hpeyton33@gmail.com>
2025-07-11 08:23:16 -0500

    creat the computer choice logic created a function that assigns a random number to via math.random to varabile computer choice logic if that random number is below or equal to 0.33 then set computerplayerchpice to rock if it is between 0.33 and 0.66 it is paper and higher then 0.66 to scissiors

---
+++ script.js
@@ -0,0 +1,17 @@
+let computerPlayerSelection ;
+
+function getComputerChoice() {
+let computerChoiceLogic = Math.random()
+if (computerChoiceLogic <= 0.33 ) {
+return computerPlayerSelection = "rock"    
+} else if (computerChoiceLogic > 0.33 && computerChoiceLogic <= 0.66) {
+  return computerPlayerSelection = "paper"  
+} else if (computerChoiceLogic > 0.55) {
+  return computerPlayerSelection = "Scissors"  
+} else { computerPlayerSelection = "Something went wrong try again"    
+} {    
+} {    
+}  
+console.log(computerPlayerSelection)
+
+}
